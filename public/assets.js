45e5ac122d54bd5f6bb99ee76e24c1b3908647e5
Lynn Oakker Zaw <167058976+Oakker52@users.noreply.github.com>
2025-02-09 19:30:46 +0630

    Add files via upload

---
+++ public/assets.js
@@ -0,0 +1,25 @@
+import img_1 from "./photo_2025-02-08_22-20-42.jpg"
+import img_2 from "./photo_2025-02-08_22-02-56.jpg"
+import bimg1 from "./photo_2025-02-08_22-02-59.jpg"
+import bimg2 from "./photo_2025-02-08_22-03-01.jpg"
+import bimg3 from "./photo_2025-02-08_22-02-57.jpg"
+import bimg4 from "./photo_2025-02-08_22-02-58.jpg"
+import fimg1 from "./photo_2025-02-08_23-18-32.png"
+import fimg2 from "./photo_2025-02-09_00-20-18.png"
+import fimg3 from "./photo_2025-02-09_00-20-20.png"
+import fimg4 from "./dogwithoutbg.png"
+import bgimg from "./photo_2025-02-08_23-18-34.jpg"
+
+export const assets = {
+    img_1,
+    img_2,
+    bimg1,
+    bimg2,
+    bimg3,
+    bimg4,
+    fimg1,
+    fimg2,
+    fimg3,
+    fimg4,
+    bgimg
+}
